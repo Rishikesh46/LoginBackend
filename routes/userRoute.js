6396ae739604343977caefe3726dc1c4776945da
Rishikesh46 <rishikesh.raut@raweng.com>
2021-01-17 19:55:30 +0530

    added signup route

---
+++ routes/userRoute.js
@@ -0,0 +1,26 @@
+const express = require("express");
+//custom inports
+const { signupUser } = require("../controllers/signUpUser");
+const { loginUser } = require("../controllers/loginUser");
+//middlewares
+const {
+  checkRequestBody,
+  isEmailValid,
+  checkConfirmPassword,
+  createPasswordHash,
+  isUserRegistered,
+} = require("../middleware/userMiddleware");
+const router = express.Router();
+router
+  .route("/signup")
+  .post(
+    checkRequestBody,
+    isEmailValid,
+    checkConfirmPassword,
+    createPasswordHash,
+    signupUser
+  );
+router.route("/login").post(checkRequestBody, isUserRegistered, loginUser);
+
+//export router
+module.exports = router;
